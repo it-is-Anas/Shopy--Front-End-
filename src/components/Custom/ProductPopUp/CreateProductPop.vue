59898b7c4ee31d02903915dd27edaace85e2862d
prontoTheLegend <anasprog8@gmail.com>
2025-04-06 00:09:56 +0300

    Product Pop Up Change to be hidden

--- src/components/Custom/ProductPopUp/CreateProductPop.vue
+++ src/components/Custom/ProductPopUp/CreateProductPop.vue
@@ -49,7 +49,7 @@ import { helpers, required , numeric } from '@vuelidate/validators';
 export default {
     setup(){
         const store = useStore();
-        const openProductPopUp = ref(1);
+        const openProductPopUp = ref(false);
 
 
 
