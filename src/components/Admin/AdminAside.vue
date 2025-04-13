7a795deca2094482ef67d8fe0db473d9e94a769a
prontoTheLegend <anasprog8@gmail.com>
2025-04-13 14:08:35 +0300

    Links added

--- src/components/Admin/AdminAside.vue
+++ src/components/Admin/AdminAside.vue
@@ -1,10 +1,18 @@
 <template>
     <aside class="admin-aside" >
         <img @click="openList" class="admin-aside_btn" src="../../assets/System/Admin/Menu.png" alt="">
-        <img class="admin-aside_btn" src="../../assets/System/Admin/home.png" alt="">
-        <img class="admin-aside_btn" src="../../assets/System/Admin/users.png" alt="">
-        <img class="admin-aside_btn" src="../../assets/System/Admin/notii.png" alt="">
-        <img class="admin-aside_btn last" src="../../assets/System/Admin/info.png" alt="">
+        <router-link class="admin-aside_btn" to="/admin/" >
+            <img class="admin-aside_btn" src="../../assets/System/Admin/home.png" alt="">
+        </router-link>
+        <router-link class="admin-aside_btn" to="/admin/users" >
+            <img class="admin-aside_btn" src="../../assets/System/Admin/users.png" alt="">
+        </router-link>
+        <router-link class="admin-aside_btn" to="/admin/notifications" >
+            <img class="admin-aside_btn" src="../../assets/System/Admin/notii.png" alt="">
+        </router-link>
+        <router-link class="admin-aside_btn last" to="/admin/info" >
+            <img class="admin-aside_btn last" src="../../assets/System/Admin/info.png" alt="">
+        </router-link>
         <div class="g-bk admin-bk" @click="closeList"   v-if="list"  ></div>
         <transition  name="left-slide" >
             <aside class="admin-aside_list" v-if="list" >
@@ -12,22 +20,22 @@
                     <img class="admin-aside_chat-img" src="../../assets/System/Admin/Menu.png" alt="">
                     <p class="admin-aside_chat-text" > Close </p>
                 </div>
-                <div class="admin-aside_list-chat">
+                <router-link to="/admin" class="admin-aside_list-chat">
                     <img class="admin-aside_chat-img" src="../../assets/System/Admin/home.png" alt="">
                     <p class="admin-aside_chat-text" > Home </p>
-                </div>
-                <div class="admin-aside_list-chat">
+                </router-link>
+                <router-link to="/admin/users" class="admin-aside_list-chat">
                     <img class="admin-aside_chat-img" src="../../assets/System/Admin/users.png" alt="">
                     <p class="admin-aside_chat-text" > users </p>
-                </div>
-                <div class="admin-aside_list-chat">
+                </router-link>
+                <router-link to="/admin/notification" class="admin-aside_list-chat">
                     <img class="admin-aside_chat-img" src="../../assets/System/Admin/notii.png" alt="">
                     <p class="admin-aside_chat-text" > Notifications </p>
-                </div>
-                <div class="admin-aside_list-chat last">
+                </router-link>
+                <router-link to="/admin/info" class="admin-aside_list-chat last">
                     <img class="admin-aside_chat-img" src="../../assets/System/Admin/info.png" alt="">
                     <p class="admin-aside_chat-text" > Info </p>
-                </div>
+                </router-link>
             </aside>
         </transition>
     </aside>
