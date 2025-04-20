6d0cf947f6de0ca88f5c61c317d4cd353d137fd1
prontoTheLegend <anasprog8@gmail.com>
2025-04-20 10:25:55 +0300

    Finish Notification Ui --admin

---
+++ src/components/Admin/NotificationCard/NotificationCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="admin-notification-card" >
+        <img  class="admin-notification-card-img" src="../../../assets/notiexp.jpg" alt="" >
+        <div class="admin-notification-card-text">
+            <h5 class="admin-notification-card-name">Admin Name</h5>
+            <p class="admin-notification-card-content">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit quas quam reiciendis id ad eum illum qid ad eum illum qid ad eum illum qid ad eum illum q 
+            </p>
+        </div>
+        <p class="admin-notification-card-date">2/2/2011 | 5:00 pm</p>
+    </div>
+</template>
--- src/views/Admin/Childreens/Notifications/AdminNoti.vue
+++ src/views/Admin/Childreens/Notifications/AdminNoti.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="admin-noti" >
-        
+        <div class="col admin-noti_col-one">
+            <h3 class="admin-noti-head-line">Previous Notifications :</h3>
+            <div class="admin-noti-prev-noti-box" >
+                <NotificationCard v-for="one in 100" :key="one" />
+            </div>
+        </div>
+        <div class="col admin-noti_col-two">
+            <h3 class="admin-noti-head-line">Create New Notification :</h3>
+            <textarea class="admin-noti-input-box" placeholder="Hello Ich bin Anas" ></textarea>
+            <div class="admin-noti-btn-box">
+                <p class="admin-fliter-box-element">Clear</p>
+                <p class="admin-fliter-box-element active">Send</p>
+            </div>
+        </div>
     </div>
 </template>
 <script>
-export default {
+import NotificationCard from '@/components/Admin/NotificationCard/NotificationCard.vue';
 
+export default {
+    components: {
+        NotificationCard,
+    }
 }
 </script>
